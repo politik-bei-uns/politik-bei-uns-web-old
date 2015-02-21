3f896df11de7101280c0b17a6b023de1e7953309
Ernesto Ruge <mail@ernestoruge.de>
2015-02-21 13:08:12 +0100

    Fix for missing add-apt-repository

--- install.sh
+++ install.sh
@@ -24,6 +24,8 @@ basepath="/srv/www"
 
 # Java Current for Elasticsearch
 echo "Installiere Oracle Java 8 für ElasticSearch"
+apt-get update
+apt-get install software-properties-common
 add-apt-repository ppa:webupd8team/java
 apt-get update
 apt-get install -y oracle-java8-installer 
