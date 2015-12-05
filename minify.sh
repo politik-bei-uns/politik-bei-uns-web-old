97b02cf73cea6f34f384e0b00fb2bc2de3cd25f9
Ernesto Ruge <mail@ernestoruge.de>
2015-12-05 19:56:11 +0100

    minification

--- minify.sh
+++ minify.sh
@@ -9,7 +9,6 @@
 /usr/bin/yui-compressor /srv/www/ris-web/webapp/static/js/file_details.js -o /srv/www/ris-web/webapp/static/js/file_details.min.js
 /usr/bin/yui-compressor /srv/www/ris-web/webapp/static/js/map.js -o /srv/www/ris-web/webapp/static/js/map.min.js
 /usr/bin/yui-compressor /srv/www/ris-web/webapp/static/js/paper_details.js -o /srv/www/ris-web/webapp/static/js/paper_details.min.js
-/usr/bin/yui-compressor /srv/www/ris-web/webapp/static/js/region.js -o /srv/www/ris-web/webapp/static/js/region.min.js
 /usr/bin/yui-compressor /srv/www/ris-web/webapp/static/js/script.js -o /srv/www/ris-web/webapp/static/js/script.min.js
 /usr/bin/yui-compressor /srv/www/ris-web/webapp/static/js/search.js -o /srv/www/ris-web/webapp/static/js/search.min.js
 
